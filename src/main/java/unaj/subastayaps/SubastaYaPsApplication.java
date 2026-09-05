748e9d7a5be6a1498a1ea79cdfd607a2300a3ee1
jenniferbogado1 <127226927+jenniferbogado1@users.noreply.github.com>
2026-09-05 17:29:43 -0300

    Initial commit

---
+++ src/main/java/unaj/subastayaps/SubastaYaPsApplication.java
@@ -0,0 +1,13 @@
+package unaj.subastayaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubastaYaPsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubastaYaPsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unaj/subastayaps/SubastaYaPsApplicationTests.java
@@ -0,0 +1,13 @@
+package unaj.subastayaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubastaYaPsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

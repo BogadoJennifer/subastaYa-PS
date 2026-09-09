d168f8ff33961d73fa984259323fd976c9b72fc2
marto <martiinleuno@gmail.com>
2026-09-09 12:04:04 -0300

    Feat: Anti-sniping and Background-Worker.

--- src/main/java/unaj/subastaya/repository/AuctionRepository.java
+++ src/main/java/unaj/subastaya/repository/AuctionRepository.java
@@ -4,6 +4,10 @@ import unaj.subastaya.model.Auction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
+    List<Auction> findByStateAndEndDateBefore(String state, LocalDateTime dateTime);
 }
---
+++ src/main/java/unaj/subastaya/repository/BidRepository.java
@@ -0,0 +1,20 @@
+package unaj.subastaya.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import unaj.subastaya.model.Bid;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BidRepository extends JpaRepository<Bid, Long> {
+
+    List<Bid> findByAuctionId(Long auctionId);
+
+    List<Bid> findByBidderId(Long bidderId);
+
+    // Consulta útil para obtener directamente la puja más alta de una subasta
+    Optional<Bid> findTopByAuctionIdOrderByAmountDesc(Long auctionId);
+
+}
